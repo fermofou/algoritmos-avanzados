3b1846b1aa79b925ef30bd33c6004107b47ecce4
fermofou <fermofou@gmail.com>
2024-10-06 14:05:40 -0600

    pt1

--- E1/main.cpp
+++ E1/main.cpp
@@ -4,13 +4,15 @@
 #include <string>
 using namespace std;
 
+//Fernando Morán        A01284623
+//Julen Hoppenstedt     
+
 int main(){
     cout<<"Hello World"<<endl;
-    string op="texts\\";
     vector<string> texts={"transmission1.txt","transmission2.txt","transmission3.txt"};
     vector<string> textSaved;
     for (string txt : texts){
-        ifstream file(op+txt);
+        ifstream file("texts\\"+txt);
         string line,t;
         while (getline(file, line)) {
             t+= line;
@@ -18,6 +20,22 @@ int main(){
         textSaved.push_back(t);
         file.close();
     }
+
+    ofstream outFile("output\\checking.txt" );
+    string ansMan="1234";
+    outFile << "Código: "+ansMan<< endl;
+    outFile<<"============== \nPalíndromo más grande:" <<endl;
+    for( string s: texts){
+        outFile<<s+"==> Posición: "<<endl;
+    }
+    outFile<<"============== \nLos Substring más largos son:" <<endl;
+
+    // for (const string &text : textSaved) {
+ 
+    //     outFile << text << endl;  // Write each text into the output file
+    // }
+
+    outFile.close();   
     
     return 0;
 }
---
+++ E1/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+or (const string &text : textSaved) {
+ 
+    //     outFile << text << endl;  // Write each text into the output file
+    // }
