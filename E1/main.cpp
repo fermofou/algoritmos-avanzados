c90588354609b7a12abca3c9c4ecf4ca2d4765f5
fermofou <fermofou@gmail.com>
2024-10-06 13:18:18 -0600

    leer files

--- E1/main.cpp
+++ E1/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-
+#include <vector>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main(){
-
+    cout<<"Hello World"<<endl;
+    string op="texts\\";
+    vector<string> texts={"transmission1.txt","transmission2.txt","transmission3.txt"};
+    vector<string> textSaved;
+    for (string txt : texts){
+        ifstream file(op+txt);
+        string line,t;
+        while (getline(file, line)) {
+            t+= line;
+        }
+        textSaved.push_back(t);
+        file.close();
+    }
+    
     return 0;
 }
