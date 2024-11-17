8a9fc29d128b0648831ca5ece7637e22549ebcb8
JulenHopp <112414883+JulenHopp@users.noreply.github.com>
2024-11-16 19:16:37 -0600

    inicializando el proyecto

---
+++ E2/main.cpp
@@ -0,0 +1,41 @@
+/*
+* Actividad integradora 2
+*
+* Autores: 
+* * Fernando Morán        A01284623
+* * Julen Hoppenstedt     A01174098
+*
+* Fecha: 16/11/2024
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+struct colonia{
+    string nombre;
+    int x;
+    int y;
+    bool central;
+
+    colonia(string _nombre, int _x, int _y, bool _central) {
+        this->nombre = _nombre;
+        this->x = _x;
+        this->y = _y;
+        this->central = _central;
+    }
+};
+
+int main() {
+    int n, m, k, q;
+    cin >> n >> m >> k >> q;
+
+    //
+
+
+    return 0;
+}
