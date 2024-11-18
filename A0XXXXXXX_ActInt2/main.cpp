46059e9e8e3173cb89b81e85b251cc10bc568e42
JulenHopp <112414883+JulenHopp@users.noreply.github.com>
2024-11-18 17:57:29 -0600

    Pregunta 3

--- A0XXXXXXX_ActInt2/main.cpp
+++ A0XXXXXXX_ActInt2/main.cpp
@@ -270,88 +270,57 @@ pair<int, vector<string>> tsp(int matAdj[MAX][MAX], int n, const vector<colonia>
     return {costoOptimo, rutaOptima};
 }
 
-pair<int, vector<string>> tspCentrales(int matAdj[MAX][MAX], int n, const vector<colonia>& colonias) {
-    int costoOptimo = INF;
-    vector<string> rutaOptima;
+// Complejidad O(n^3)
+pair<vector<vector<int>>, vector<vector<int>>> floyd(int matAdj[MAX][MAX], int nodes) {
+    vector<vector<int>> dist(nodes + 1, vector<int>(nodes + 1, INF)); // Copia matriz con valores originales
 
-    // Encontrar el primer nodo central
-    int nodoInicial = -1;
-    for (int i = 1; i <= n; i++) {
-        if (colonias[i - 1].central) {
-            nodoInicial = i;
-            break;
-        }
-    }
+    vector<vector<int>> rutas(nodes + 1, vector<int>(nodes + 1, -1)); // Matriz de rutas
 
-    if (nodoInicial == -1) {
-        cerr << "No hay nodos centrales disponibles" << endl;
-        return {INF, {}};
-    }
-
-    // Inicializar el nodo raíz
-    tspNode raiz(n + 1);
-    raiz.niv = 1;
-    raiz.costoAcum = 0;
-    raiz.verticeActual = nodoInicial;
-    raiz.visitados[nodoInicial] = true;
-    raiz.ruta.push_back(colonias[nodoInicial - 1].nombre);
-
-    calculaCostoPosible(raiz, matAdj, n);
-
-    priority_queue<tspNode> pq;
-    pq.push(raiz);
-
-    while (!pq.empty()) {
-        tspNode current = pq.top();
-        pq.pop();
-
-        // Podar ramas con costos posibles mayores al óptimo actual
-        if (current.costoPos >= costoOptimo) {
-            continue;
-        }
-
-        // Verificar si todas las colonias centrales han sido visitadas
-        bool todasVisitadas = true;
-        for (int i = 1; i <= n; i++) {
-            if (colonias[i - 1].central && !current.visitados[i]) {
-                todasVisitadas = false;
-                break;
+    // Inicializando matrices
+    for (int i = 1; i <= nodes; i++) {
+        for (int j = 1; j <= nodes; j++) {
+            dist[i][j] = matAdj[i][j];
+            if (matAdj[i][j] != INF && i != j) {
+                rutas[i][j] = j; // Inicialmente, el siguiente nodo es el destino directo
             }
         }
+    }
 
-        if (todasVisitadas) {
-            // Calcular el costo para regresar al nodo inicial
-            int costoDeVuelta = matAdj[current.verticeActual][nodoInicial];
-            if (costoDeVuelta != INF) {
-                int costoTotal = current.costoAcum + costoDeVuelta;
-                if (costoTotal < costoOptimo) {
-                    costoOptimo = costoTotal;
-                    rutaOptima = current.ruta;
-                    rutaOptima.push_back(colonias[nodoInicial - 1].nombre); // Regresar al nodo inicial
+    // Algoritmo de Floyd-Warshall
+    for (int k = 1; k <= nodes; k++) {
+        for (int i = 1; i <= nodes; i++) {
+            for (int j = 1; j <= nodes; j++) {
+                if (dist[i][k] != INF && 
+                    dist[k][j] != INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                    rutas[i][j] = rutas[i][k]; // Actualizar el siguiente nodo
                 }
             }
-            continue;
-        }
-
-        // Expandir nodos hijos
-        for (int i = 1; i <= n; i++) {
-            if (!current.visitados[i] && matAdj[current.verticeActual][i] != INF) {
-                tspNode newNode = current;
-                newNode.niv++;
-                newNode.verticeActual = i;
-                newNode.costoAcum += matAdj[current.verticeActual][i];
-                newNode.visitados[i] = true;
-                newNode.ruta.push_back(colonias[i - 1].nombre);
-
-                calculaCostoPosible(newNode, matAdj, n);
-
-                pq.push(newNode);
-            }
         }
     }
 
-    // Retornar el costo óptimo y la ruta
-    return {costoOptimo, rutaOptima};
+    return {dist, rutas};
+}
+
+vector<int> reconstruirCamino(int start, int end, const vector<vector<int>>& rutas) {
+    vector<int> path;
+
+    if (rutas[start][end] == -1) {
+        // No hay camino entre los nodos
+        return path;
+    }
+
+    int current = start;
+    path.push_back(current);
+
+    while (current != end) {
+        current = rutas[current][end];
+        path.push_back(current);
+    }
+
+    return path;
 }
 
 string coloniaMasCercana(colonia nuevaColonia, const vector<colonia>& colonias) {
@@ -465,23 +434,31 @@ int main() {
 
 
     // // Pregunta 3
-    out << "3 - Caminos más cortos entre centrales." << endl;
+    out << "3 - Caminos más cortos entre centrales." << endl << endl;
 
-    pair<int, vector<string>> resultCentrales = tspCentrales(g.matAdj, n, colonias);
+    pair<vector<vector<int>>, vector<vector<int>>> conexiones = floyd(g.matAdj, n);
 
-    if (resultCentrales.first != INF) {
-        // Print the route
-        for (int i = 0; i < resultCentrales.second.size(); i++) {
-            out << resultCentrales.second[i];
-            if (i != resultCentrales.second.size() - 1)
-                out << " - ";
+    for (int i = 0; i <= n; i++) { // Ajustar índices: matriz inicia en 1
+        for (int j = i; j <= n; j++) {
+            if (i == j) continue;
+            if (!colonias[i].central || !colonias[j].central) continue;
+            if (conexiones.first[i + 1][j + 1] == INF) continue;
+
+            vector<int> camino = reconstruirCamino(i + 1, j + 1, conexiones.second);
+            string resultado;
+
+            for (size_t k = 0; k < camino.size(); k++) {
+                resultado += colonias[camino[k] - 1].nombre;
+                if (k != camino.size() - 1) {
+                    resultado += " - ";
+                }
+            }
+
+            out << resultado << " (" << conexiones.first[i + 1][j + 1] << ")" << endl;
         }
-        out << " (" << resultCentrales.first << ")" << endl << endl;
-    } else {
-        out << "No se encontró una solución factible." << endl << endl;
     }
 
-    out << "-------------------" << endl;
+    out << endl << "-------------------" << endl;
 
     // Pregunta 4
     out << "4 - Conexión de nuevas colonias." << endl;
@@ -517,9 +494,9 @@ Independencia 180 -15
 Roble 45 68
 
 5 8 1 2
-LindaVista 200 120 0
+LindaVista 200 120 1
 Purisima 150 75 0
-Tecnologico -50 20 0
+Tecnologico -50 20 1
 Roma -75 50 0
 AltaVista -50 40 0
 LindaVista Purisima 48
@@ -533,5 +510,41 @@ Roma AltaVista 18
 Purisima Tecnologico
 Independencia 180 -15
 Roble 45 68
+
+10 19 0 4
+Condesa -193 -151 1
+DelValle -142 -70 0
+Polanco 144 -122 1
+RomaNorte -91 -160 1
+Pantitlán -108 42 0
+Juárez -97 127 1
+Tlatelolco -160 12 0
+SantaFe 169 -91 1
+Cosmopolita 160 -67 1
+Merced 78 16 0
+Condesa Polanco 27
+Condesa Juárez 68
+Condesa Cosmopolita 34
+DelValle RomaNorte 80
+DelValle Pantitlán 68
+DelValle SantaFe 94
+DelValle Cosmopolita 86
+Polanco Juárez 92
+Polanco Tlatelolco 58
+Polanco Merced 54
+RomaNorte Juárez 69
+RomaNorte Cosmopolita 25
+Pantitlán Juárez 4
+Pantitlán SantaFe 67
+Pantitlán Cosmopolita 25
+Juárez SantaFe 78
+Juárez Cosmopolita 29
+Tlatelolco Cosmopolita 82
+SantaFe Merced 66
+FelipeAngeles -64 25
+Tlazintla -85 -106
+Penitenciaria -99 -83
+Porvenir -95 -196
+
 
 */
--- A0XXXXXXX_ActInt2/tempCodeRunnerFile.cpp
+++ A0XXXXXXX_ActInt2/tempCodeRunnerFile.cpp
@@ -1,16 +1,34 @@
-    // cout << "3 - Caminos más cortos entre centrales." << endl;
-
-    // pair<int, vector<int>> resultCentrales = tspCentrales(g.matAdj, n, colonias);
-
-    // if (resultCentrales.first != INF) {
-    //     // Print the route
-    //     for (size_t i = 0; i < resultCentrales.second.size(); i++) {
-    //         int idx = resultCentrales.second[i];
-    //         cout << colonias[idx].nombre;
-    //         if (i != resultCentrales.second.size() - 1)
-    //             cout << " - ";
-    //     }
-    //     cout << " (" << resultCentrales.first << ")" << endl;
-    // } else {
-    //     cout << "No se encontró una solución factible." << endl;
-    // }
+10 19 0 4
+Condesa -193 -151 1
+DelValle -142 -70 0
+Polanco 144 -122 1
+RomaNorte -91 -160 1
+Pantitlán -108 42 0
+Juárez -97 127 1
+Tlatelolco -160 12 0
+SantaFe 169 -91 1
+Cosmopolita 160 -67 1
+Merced 78 16 0
+Condesa Polanco 27
+Condesa Juárez 68
+Condesa Cosmopolita 34
+DelValle RomaNorte 80
+DelValle Pantitlán 68
+DelValle SantaFe 94
+DelValle Cosmopolita 86
+Polanco Juárez 92
+Polanco Tlatelolco 58
+Polanco Merced 54
+RomaNorte Juárez 69
+RomaNorte Cosmopolita 25
+Pantitlán Juárez 4
+Pantitlán SantaFe 67
+Pantitlán Cosmopolita 25
+Juárez SantaFe 78
+Juárez Cosmopolita 29
+Tlatelolco Cosmopolita 82
+SantaFe Merced 66
+FelipeAngeles -64 25
+Tlazintla -85 -106
+Penitenciaria -99 -83
+Porvenir -95 -196
